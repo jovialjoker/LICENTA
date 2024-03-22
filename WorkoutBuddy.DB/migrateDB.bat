ba5e17b595b4224f5d569807aa6a20f8bd01244b
Florea George <florgeo_02@yahoo.com>
2024-03-22 22:11:41 +0200

    added db project for migrations

---
+++ WorkoutBuddy.DB/migrateDB.bat
@@ -0,0 +1,15 @@
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+
+set DB_NAME=WorkoutBuddy
+set SQL_FILE=./wbMigration.sql
+set SERVER_NAME=(local)
+
+echo Creating database %DB_NAME%...
+sqlcmd -S %SERVER_NAME% -E -Q "CREATE DATABASE [%DB_NAME%];"
+
+echo Importing %SQL_FILE% into %DB_NAME%...
+sqlcmd -S %SERVER_NAME% -d %DB_NAME% -E -i %SQL_FILE%
+
+echo Migration completed successfully.
+ENDLOCAL
